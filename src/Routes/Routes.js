819fb3f9cdf9ae3facdd8dececf0d230f827914a
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-12-27 10:48:52 +0600

    router setup

---
+++ src/Routes/Routes.js
@@ -0,0 +1,18 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../layout/Main");
+const { default: Home } = require("../Pages/Home/Home");
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children:[
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
