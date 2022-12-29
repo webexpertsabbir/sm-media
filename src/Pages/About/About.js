16ee5ae8b4929cd35313069dc7c484bf03d8c80f
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-12-29 22:37:30 +0600

    aboute modal design

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -27,22 +27,38 @@ const About = () => {
     return (
         <div className='max-w[1240px] mx-auto'>
             <div className='flex justify-center'>
-                <div className='md:w-1/2 p-7 border rounded py-48'>
-                    <div className='text-center'>
+                <div className='md:w-1/2 p-7 border rounded'>
+                    {/* The button to open modal */}
+                    <div className='flex justify-end'>
+                        <label htmlFor="my-modal-3" className="btn">Edit</label>
+                    </div>
+                    {/* Put this part before </body> tag */}
+                    <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box relative">
+                            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                            <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+                            <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        </div>
+                    </div>
 
-                        <div>
-                            {
-                                about.map((aboute, ids) => <div
-                                    key={ids}
-                                >
-                                    <h2 className='text-2xl'><span className='font-semibold'>Name:</span> {aboute.name}</h2>
-                                    <h2 className='pt-5'><span className='font-semibold'>Email: </span>{aboute.email}</h2>
-                                    <h2 className='py-2'><span className='font-semibold'>University: </span> {aboute.university}</h2>
-                                    <h2><span className='font-semibold'>District: </span> {aboute.district}</h2>
-                                    <h2 className='py-2'><span className='font-semibold'>Upojela:  </span>{aboute.upojela}</h2>
-                                    <h2><span className='font-semibold'>Village: </span> {aboute.village}</h2>
-                                </div>)
-                            }
+                    <div className='pb-48 pt-24'>
+                        <div className='text-center'>
+
+                            <div>
+                                {
+                                    about.map((aboute, ids) => <div
+                                        key={ids}
+                                    >
+                                        <h2 className='text-2xl'><span className='font-semibold'>Name:</span> {aboute.name}</h2>
+                                        <h2 className='pt-5'><span className='font-semibold'>Email: </span>{aboute.email}</h2>
+                                        <h2 className='py-2'><span className='font-semibold'>University: </span> {aboute.university}</h2>
+                                        <h2><span className='font-semibold'>District: </span> {aboute.district}</h2>
+                                        <h2 className='py-2'><span className='font-semibold'>Upojela:  </span>{aboute.upojela}</h2>
+                                        <h2><span className='font-semibold'>Village: </span> {aboute.village}</h2>
+                                    </div>)
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
